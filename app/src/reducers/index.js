0c439f1428fdb6801299f6e8ed748914df3a1fd4
gincheong <gincheong2@gmail.com>
2020-12-21 21:01:32 +0900

    Edit reducer name

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -3,7 +3,7 @@ import { taskReducer } from './reducer_lists';
 
 const rootReducer = combineReducers({
   // 여러 개의 리듀서를 하나로 통합해서 사용할 수 있음
-  task: taskReducer
+  taskReducer
 });
 
 export default rootReducer;
