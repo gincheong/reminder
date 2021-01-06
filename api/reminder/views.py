6db38cd8ad3ec323a914296764aaf15c85a86354
gincheong <gincheong2@gmail.com>
2021-01-06 16:32:01 +0900

    delete unused code

--- api/reminder/views.py
+++ api/reminder/views.py
@@ -1,6 +1,4 @@
 from django.db.models import Case, When, Value, IntegerField
-from django.shortcuts import render
-from django.http import HttpResponse
 
 from rest_framework import viewsets
 from rest_framework.response import Response
