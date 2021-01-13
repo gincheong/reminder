d8afbe6e4177afbf622662c0b0acdbf5a65e527e
gincheong <gincheong2@gmail.com>
2021-01-13 23:33:39 +0900

    Highlight expired task date

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -32,11 +32,14 @@ class Card extends Component {
 
   renderCard () {
     const { task_list } = this.props.card.taskReducer;
+    const tempDate = new Date();
+    const todayDate = new Date(Date.UTC(tempDate.getFullYear(), tempDate.getMonth(), tempDate.getDate()));
     return task_list.map((each) => {
       if (each.alarm) {
         each.alarm = each.alarm.replace('T', ' ').substr(0, 16);
       }
-
+      const task_date = new Date(each.task_date);
+      
       return (
         <div key={each.id} className="task-card">
           <div className="task-card-header" onClick={() => this.toggleModal(each.id)}>
@@ -44,14 +47,17 @@ class Card extends Component {
               {each.title}
             </div>
             <div className="task-summary">
-              {each.task_date ? (
-                <><i className="fas fa-calendar"></i>{each.task_date}</>)
-                : undefined}
+              {
+                !each.task_date
+                ? undefined
+                : (task_date < todayDate
+                  ? ( <><i className="fas fa-calendar"></i><span className="font-red">{each.task_date}</span></> )
+                  : ( <><i className="fas fa-calendar"></i><span>{each.task_date}</span></> )
+                )
+              }
               {each.alarm ? (
-                <><i className="fas fa-bell"></i>{each.alarm}</>)
+                <><i className="fas fa-bell"></i><span>{each.alarm}</span></>)
                 : undefined}
-              {/* // TODO: datetime 출력 형식 바꾸기 */}
-              <br/>
             </div>
           </div>
         </div>
