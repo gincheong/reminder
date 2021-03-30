e8d094b2d754f3854f470b3e44055a95faf7992d
gincheong <gincheong2@gmail.com>
2021-03-30 11:46:05 +0900

    feat(ModalButton): 동작방식 변경
    DoubleClick 방식에서, 독립적인 2회 클릭으로 버튼이 동작하도록 변경
    DeleteIcon Component의 작동방식과 동일
    
    close #4

--- app/src/components/CardModal/ModalButton/ModalButton.js
+++ app/src/components/CardModal/ModalButton/ModalButton.js
@@ -3,24 +3,33 @@ import React, { useRef } from 'react';
 import './ModalButton.scss';
 
 const ModalButton = (props) => {
-  const buttonRef = useRef();
+  const beforeClickRef = useRef();
+  const afterClickRef = useRef();
 
-  const colorAnimation = () => {
-    buttonRef.current.classList.add(props.animationname);
+  const onClick = () => {
+    if (beforeClickRef.current.classList.contains('click')) {
+      props.action();
+    } else {
+      beforeClickRef.current.classList.add('click');
+      afterClickRef.current.classList.add('click');
+    }
   };
 
-  const onAnimationEnd = (event) => {
-    event.target.classList.remove(props.animationname);
+  const onMouseLeave = () => {
+    beforeClickRef.current.classList.remove('click');
+    afterClickRef.current.classList.remove('click');
   };
 
   return (
-    <button type="button" className="modal-button"
-            onClick={props.onClick ? props.onClick : colorAnimation}
-            onDoubleClick={props.onDoubleClick}
-            onAnimationEnd={onAnimationEnd}
-            ref={buttonRef}
-    >
-      {props.children}
+    <button type="button" className="ModalButton" onClick={onClick}
+      onMouseLeave={onMouseLeave}>
+      <div className="BeforeClick" ref={beforeClickRef}>
+        {props.beforeClick}
+      </div>
+      <div className="AfterClick" ref={afterClickRef}
+        style={{color: props.color}}>
+        {props.afterClick}
+      </div>     
     </button>
   );
 };
