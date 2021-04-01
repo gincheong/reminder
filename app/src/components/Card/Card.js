e1a9ee2d7ea36b3cf56b2225c96e86d0be4a35db
gincheong <gincheong2@gmail.com>
2021-04-01 18:32:25 +0900

    feat(Card): add drag function to toggle 'Completed' ...
    'completed' 상태를 토글하는 '좌->우' 드래그 기능 추가
    delete 기능은 '우->좌' 드래그로 유지
    토글 기능이 드래그로 추가되어 'Checkbox' 제거함
    
    드래그 배경 효과를 위한 Background element 추가
    드래그 시 뒷 배경색 변하는 효과 추가 (toggle: blue, delete: red)
    
    Card 요소의 1/4 크기만큼 드래그하면 드래그가 멈추게 처리함
    
    close #32

--- app/src/components/Card/Card.js
+++ app/src/components/Card/Card.js
@@ -2,49 +2,66 @@ import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { deleteTask, fetchAllTask, updateTask } from 'actions';
-import { DoubleButton } from 'components/shared';
-// import { DeleteIcon } from './DeleteIcon/DeleteIcon';
 import './Card.scss';
 
 const Card = (props) => {
   const dispatch = useDispatch();
   const cardRef = useRef();
+  const backgroundRef = useRef();
   
   const current = new Date();
   const todayDate = new Date(Date.UTC(current.getFullYear(), current.getMonth(), current.getDate()));
   const task_date = new Date(props.data.task_date);
 
-  const onChangeHandler = (event) => {
-    event.target.checked = event.target.checked ? true : false;
-    
-    const data = new FormData();
-    data.append('title', props.data.title);
-    data.append('completed', event.target.checked);
-    dispatch(updateTask(props.data.id, data)).then(() => {
-      dispatch(fetchAllTask());
-    });
-  };
-
+  const baseColor = 46;
   // Card Drag Event
   const onMouseDown = (event) => {
+    const dragRatio = 4; // 카드의 1/4 만큼 드래그하면 기능 작동
     const startX = event.clientX;
     
+    // 드래그에 따라 element 이동시킴
     const onMouseMove = (event) => {
-      // 드래그에 따라 element 이동시킴
-      cardRef.current.style.left = event.clientX - startX + 'px';
+      // 드래그 시, 항목이 일정 이상 움직이지 않게 함
+      const moveDistance = event.clientX - startX;
+      if (Math.abs(moveDistance) <= parseInt(cardRef.current.offsetWidth / dragRatio)) {
+        cardRef.current.style.left = event.clientX - startX + 'px';
+        let [r, g, b] = '';
+        if (moveDistance > 0) {
+          [r, g, b] = [
+            baseColor + moveDistance * 0.1,
+            baseColor + moveDistance * 0.1,
+            baseColor + moveDistance * 0.9,
+          ];
+        } else {
+          [r, g, b] = [
+            baseColor + Math.abs(moveDistance) * 0.9,
+            baseColor + Math.abs(moveDistance) * 0.1,
+            baseColor + Math.abs(moveDistance) * 0.1,
+          ];
+        }
+        backgroundRef.current.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      }
     };
 
     const onMouseUp = (event) => {
+      // 이벤트 중복등록 방지
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
       
+      // 제자리 클릭 시 Modal 활성화
       if (event.clientX === startX) {
         props.toggleModal(props.data.id);
       }
 
-      const moveDistance = Math.abs(event.clientX - startX);
-      if (cardRef.current.offsetWidth / 2.7 < moveDistance) {
-        dragAnimation(event.clientX);        
+      const moveDistance = event.clientX - startX;
+      if (cardRef.current.offsetWidth / dragRatio < Math.abs(moveDistance)) {
+        if (moveDistance > 0) {
+          // left to right
+          toggleCompleted();
+        } else {
+          // right to left
+          dragAnimation(deleteCard);
+        }
       } else {
         cardRef.current.style.left = 0;
       }
@@ -55,13 +72,27 @@ const Card = (props) => {
     document.addEventListener('mouseup', onMouseUp);
   };
 
-  const dragAnimation = () => {
-    if (parseInt(cardRef.current.style.left) < cardRef.current.offsetWidth) {
-      cardRef.current.style.left = parseInt(cardRef.current.style.left) + 50 + 'px'; // ?: 숫자 넣어도 되나
-      requestAnimationFrame(dragAnimation);
+  const dragAnimation = (callback) => {
+    const dragValue = 50;
+
+    if (Math.abs(parseInt(cardRef.current.style.left)) < cardRef.current.offsetWidth) {
+      // ?: 이렇게 고정 숫자로 이동시키는게 맞나
+      cardRef.current.style.left = parseInt(cardRef.current.style.left) - dragValue + 'px';
+      requestAnimationFrame(() => { dragAnimation(callback) });
     } else {
-      deleteCard();
+      callback();
     }
+  };
+
+  const toggleCompleted = () => {
+    let completed = !cardRef.current.classList.contains('Completed');
+    
+    const data = new FormData();
+    data.append('title', props.data.title);
+    data.append('completed', completed);
+    dispatch(updateTask(props.data.id, data)).then(() => {
+      dispatch(fetchAllTask());
+    });
   };
 
   const deleteCard = () => {
@@ -71,50 +102,38 @@ const Card = (props) => {
   };
 
   return (
-    <article className={
-      props.data.completed ? 
-        "Card Completed"
-      :
-        "Card"
-    } ref={cardRef}>
-      <div className="CardCheckBox">
-        <input type="checkbox" defaultChecked={props.data.completed} onChange={onChangeHandler} />
+    <article className="Card">
+      <div className="Background" ref={backgroundRef}>
       </div>
-      <div className="CardInfo"
-        onMouseDown={props.data.completed ? onMouseDown : undefined}
-      >
-        <header className="CardTitle">
-          { props.data.title }
-        </header>
-        <section className="CardSummary">
-          { props.data.task_date &&
-            <>
-              <i className="fas fa-calendar"></i>
-              <span className={
-                task_date - todayDate === 0 ?
-                  "Orange"
-                : task_date < todayDate ?
-                  "Red"
-                : 
-                  ""
-              }
-              >{ props.data.task_date }</span>
-            </>
-          }
-          { props.data.alarm &&
-            <>
-              <i className="fas fa-bell"></i>
-              <span>{props.data.alarm.replace('T', ' ').substr(0, 16)}</span>
-            </>
-          }
-        </section>
+      <div className={"CardContent" + (props.data.completed ? " Completed" : '')} ref={cardRef}>
+        <div className="CardInfo" onMouseDown={onMouseDown}>
+          <header className="CardTitle">
+            { props.data.title }
+          </header>
+          <section className="CardSummary">
+            { props.data.task_date &&
+              <>
+                <i className="fas fa-calendar"></i>
+                <span className={
+                  task_date - todayDate === 0 ?
+                    "Orange"
+                  : task_date < todayDate ?
+                    "Red"
+                  : 
+                    ""
+                }
+                >{ props.data.task_date }</span>
+              </>
+            }
+            { props.data.alarm &&
+              <>
+                <i className="fas fa-bell"></i>
+                <span>{props.data.alarm.replace('T', ' ').substr(0, 16)}</span>
+              </>
+            }
+          </section>
+        </div>
       </div>
-      {/* { props.data.completed &&
-        <DoubleButton action={deleteCard} color="#d9598c" 
-          beforeClick={<i className="fas fa-times"></i>}
-          afterClick={<i className="fas fa-trash-alt"></i>}
-        />
-      } */}
     </article>
   );
 };
